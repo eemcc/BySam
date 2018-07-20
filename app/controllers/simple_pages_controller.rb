185e3d2dc6fb5f3b1b1fbefb9177cca37f77405d
Elizabeth <eemccracken@gmail.com>
2018-07-19 21:48:27 -0400

    Order page

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,9 +1,5 @@
 class SimplePagesController < ApplicationController
   def landing_page
-    unless (product.name==nil)
-      puts "No featured product"
-    else
-      @featuring_product = Product.first
-    end
+    @featured_product = Product.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   root 'simple_pages#landing_page'
+  resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
