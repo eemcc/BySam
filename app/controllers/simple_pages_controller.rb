30d2e80e29d55605095df93953b317a628d11401
Elizabeth <eemccracken@gmail.com>
2018-07-22 18:32:04 -0400

    Landing Carousel

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,6 +1,6 @@
 class SimplePagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
-    @product = Product.limit(3)
+    @products = Product.limit(3)
   end
 end
