6a2a27e9071b5813f5fb63000d63c060510000a9
Elizabeth <eemccracken@gmail.com>
2018-07-29 22:28:14 -0400

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  root 'simple_pages#landing_page'
+  get 'simple_pages/landing_page'
+  root 'simple_pages#about'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
