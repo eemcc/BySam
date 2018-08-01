908add86c813ba5a3c11893d5fa1d3ef65dfb970
Elizabeth <eemccracken@gmail.com>
2018-08-01 19:55:32 -0400

    forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/landing_page'
-  root 'simple_pages#about'
+  root 'simple_pages#landing_page'
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
