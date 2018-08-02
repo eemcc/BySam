68601387ece4e5370721eeae94bec4f6ce5c46d8
Elizabeth <eemccracken@gmail.com>
2018-08-02 15:36:13 -0400

    Email Sendgrid

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,4 +3,13 @@ class SimplePagesController < ApplicationController
     @featured_product = Product.first
     @products = Product.limit(3)
   end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+      to: 'eemccracken@gmail.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+  end
 end
