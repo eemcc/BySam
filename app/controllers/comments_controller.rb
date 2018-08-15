96c82bfb31666f9cc36631c744e47a27941907f8
Elizabeth <eemccracken@gmail.com>
2018-08-15 12:51:19 -0400

    pagination and validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,16 @@ class CommentsController < ApplicationController
     redirect_to product_path(@product)
   end
 
+  respond_to do |format|
+    if @comment.save
+      format.html { redirect_to @product, notice: 'Review was created successfully.'}
+      format.json { render :show, status: :created, location: @product }
+    else
+      format.html { redirect_to @product, alert: 'Review was not saved successfully.'}
+      format.json { render json: @comments.errors, status: :unprocessable_entity }
+    end
+  end
+
   def destroy
   end
 
