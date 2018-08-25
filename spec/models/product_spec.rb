fecfc6393798f3521d9b473dfa05cce7c2d44674
Elizabeth <eemccracken@gmail.com>
2018-08-25 10:46:47 -0400

    average rating test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,18 +1,22 @@
 require 'rails_helper'
 
 describe Product do
-  let(:product) { Product.create!(name: "White Monkeys Fist") }
-  let(:user) { User.create!(email: "eemccracken@gmail.com", password: "Testing") }
+  context "when the product has comments" do
+    let(:product) { Product.create!(name: "White Monkeys Fist") }
+    let(:user) { User.create!(email: "eemccracken@gmail.com", password: "Testing") }
 
-  before do
-    product.comments.create!(rating: 1, user: user, body: "Awful!")
-    product.comments.create!(rating: 3, user: user, body: "Ok!")
-    product.comments.create!(rating: 5, user: user, body: "Great!")
+    before do
+      product.comments.create!(rating: 1, user: user, body: "Awful!")
+      product.comments.create!(rating: 3, user: user, body: "Ok!")
+      product.comments.create!(rating: 5, user: user, body: "Great!")
+    end
+
+    it "returns the average rating of all comments" do
+      expect(Product.average_rating).to eq 3
+    end
+
+    it "is not valid without a name" do
+      expect(Product.new(description: "Nice Earrings")).not_to be_valid
+    end
   end
-
-  it "returns the average rating of all comments" do
-
-  end
-
-  expect(Product.new(description: "Nice Earrings")).not_to_be_valid
 end
