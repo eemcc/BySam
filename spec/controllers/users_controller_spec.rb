67592db9b56edbf91c082fdc0dce7ed9c5e4e26f
Elizabeth <eemccracken@gmail.com>
2018-08-28 23:56:46 -0400

    Deleted tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,18 +6,12 @@ describe UsersController, type: :controller do
   describe 'GET #show' do
     context 'when a user is logged in' do
       before do
-        'sign_in user'
+        sign_in user
       end
       it 'loads correct user details' do
         get :show, params: { id: user.id }
         expect(response).to have_http_status(200)
         expect(assigns(:user)).to eq user
-      end
-    end
-    context 'when a user is not logged in' do
-      it 'redirects to login' do
-        get :show, params: { id: user.id }
-        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
